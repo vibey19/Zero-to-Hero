57415fc7429d4197b723a969bd36c7b453a9dea3
Jainil Dadhaniya <jdadhaniya6@gmail.com>
2025-01-02 11:40:23 +0530

    completed Header Component with web3auth

--- utils/db/dbConfig.jsx
+++ utils/db/dbConfig.jsx
@@ -1,5 +1,10 @@
 import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
-const sql = neon(process.env.DATABASE_URL);
+import dotenv from "dotenv";
+dotenv.config();
+
+const sql = neon(
+  "postgresql://neondb_owner:ArtnF6gGDN9p@ep-shy-bird-a5tkrd72.us-east-2.aws.neon.tech/zero2hero?sslmode=require"
+);
 export const db = drizzle(sql, { schema });
